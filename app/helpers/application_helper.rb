137d9b088f3dc4f8d03928dc6abfa6f3d6b9fb72
Daniel Collis-Puro <dan@collispuro.net>
2011-03-16 10:30:35 -0400

    Document display.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def icon(icon_name = 'application', alt = 'icon')
-    image_tag("icons/#{icon_name}.png", :alt => alt)
+    image_tag("icons/#{icon_name}.png", :alt => alt, :class => "icon")
   end
 end
