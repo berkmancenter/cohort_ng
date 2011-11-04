c5ffbec043df7dce56246a6befe9bd7250a020f6
djcp <dan@collispuro.com>
2011-11-04 08:16:02 -0400

    Document refactoring.

--- app/controllers/document_query_controller.rb
+++ app/controllers/document_query_controller.rb
@@ -1,15 +1,24 @@
-class DocumentQueryController < ApplicationController
+class DocumentQueryController < BaseController
+
   def new
+    @documents = Document.paginate(:order => 'created_at desc', :page => params[:page], :per_page => params[:per_page] || Document.per_page)
+    negotiate_list_query_response('document')
   end
 
   def recent
+    @documents = Document.paginate(:order => 'updated_at desc', :page => params[:page], :per_page => params[:per_page] || Document.per_page)
+    negotiate_list_query_response('document')
   end
 
   def yours
+    if current_user
+      @documents = Document.joins(:accepted_roles => [:users]).paginate(:conditions => ['roles.name = ? and roles.authorizable_type = ? and roles_users.user_id = ?','owner','Note', current_user.id], :page => params[:page], :per_page => params[:per_page] || Document.per_page)
+    end
+    negotiate_list_query_response('document')
   end
 
   def contact
-    @documents = Document.paginate(:conditions => ['contact_id = ?', params[:id]], :order => 'created_at', :page => params[:page], :per_page => params[:per_page])
+    @documents = Document.paginate(:conditions => ['contact_id = ?', params[:id]], :order => 'created_at', :page => params[:page], :per_page => params[:per_page] || Document.per_page)
     respond_to do |format|
       format.js {
         render :partial => "shared/contact_document_list"
--- app/controllers/note_query_controller.rb
+++ app/controllers/note_query_controller.rb
@@ -1,6 +1,6 @@
 class NoteQueryController < BaseController
   def new
-    @notes = Note.paginate(:order => 'created_at desc', :page => params[:page], :per_page => params[:per_page])
+    @notes = Note.paginate(:order => 'created_at desc', :page => params[:page], :per_page => params[:per_page] || Note.per_page)
     negotiate_list_query_response('note')
   end
 
@@ -11,13 +11,13 @@ class NoteQueryController < BaseController
 
   def upcoming
     if current_user
-      @notes = Note.to_dos.joins(:accepted_roles => [:users]).paginate(:conditions => ['roles.name = ? and roles.authorizable_type = ? and roles_users.user_id = ?','owner','Note', current_user.id], :order => 'due_date desc', :page => params[:page], :per_page => params[:per_page])
+      @notes = Note.to_dos.joins(:accepted_roles => [:users]).paginate(:conditions => ['roles.name = ? and roles.authorizable_type = ? and roles_users.user_id = ?','owner','Note', current_user.id], :order => 'due_date desc', :page => params[:page], :per_page => params[:per_page] || Note.per_page)
     end
     negotiate_list_query_response('note')
   end
 
   def all_upcoming
-    @notes = Note.to_dos.paginate(:order => 'due_date desc', :page => params[:page], :per_page => params[:per_page])
+    @notes = Note.to_dos.paginate(:order => 'due_date desc', :page => params[:page], :per_page => params[:per_page] || Note.per_page)
     negotiate_list_query_response('note')
   end
 
@@ -30,7 +30,7 @@ class NoteQueryController < BaseController
 
 
   def contact
-    @notes = Note.paginate(:conditions => ['contact_id = ?', params[:id]], :order => 'created_at', :page => params[:page], :per_page => params[:per_page])
+    @notes = Note.paginate(:conditions => ['contact_id = ?', params[:id]], :order => 'created_at', :page => params[:page], :per_page => params[:per_page] || Note.per_page)
     respond_to do |format|
       format.js {
         render :partial => "shared/contact_note_list"
