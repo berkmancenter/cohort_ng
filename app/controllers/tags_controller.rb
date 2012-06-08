807ef29afdeaa71ae09ff8ec43e203715c584a18
djcp <dan@collispuro.net>
2012-06-08 17:03:16 -0400

    Tag merge and destroy.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -21,11 +21,30 @@ class TagsController < BaseController
   end
 
   def merge
-    original_tag = ActsAsTaggableOn::Tag.find(params[:tag_id])
+    tag = ActsAsTaggableOn::Tag.find(params[:id])
     new_tag = ActsAsTaggableOn::Tag.find(params[:new_tag_id])
-
-
-
+    contacts_to_reindex = []
+    tag.taggings.each do |tg|
+      contacts_to_reindex << tg.taggable.id
+      if tg.taggable.tags.include?(new_tag)
+        tg.destroy
+      else
+        tagging = ActsAsTaggableOn::Tagging.new(:taggable => tg.taggable, :tag => new_tag, :context => 'tags')
+        tagging.save
+        tg.destroy
+      end
+    end
+    Contact.where(:id => contacts_to_reindex).solr_index
+    new_tag.solr_index
+    if params[:delete].to_i == 1
+      # Hasta la vista, baby.
+      tag.destroy
+    else
+      # Just reindex.
+      tag.solr_index
+    end
+    flash[:notice] = 'Merged.'
+    redirect_to acts_as_taggable_on_tag_path(new_tag) and return
   end
 
   def new
@@ -41,6 +60,12 @@ class TagsController < BaseController
   end
 
   def destroy
+    tag = ActsAsTaggableOn::Tag.find(params[:id])
+    contacts_to_reindex = tag.taggings.collect{|tg| tg.taggable.id}
+    tag.destroy
+    Contact.where(:id => contacts_to_reindex).solr_index
+    flash[:notice] = 'We deleted that tag.'
+    redirect_to tags_path and return
   end
 
 end
